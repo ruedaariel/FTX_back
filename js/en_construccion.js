a2eaa2828fa2ded305eadf69db7a3982ab9bf86c
gladysherrera <profherreratic@gmail.com>
2025-05-21 17:49:58 -0300

    cambios en_construccion.html y js

---
+++ js/en_construccion.js
@@ -0,0 +1,9 @@
+
+  document.getElementById("volver").addEventListener("click", function () {
+    if (document.referrer) {
+      window.location.href = document.referrer; // Redirige a la página anterior
+    } else {
+      window.location.href = "../index.html"; // Página por defecto si no hay referrer
+    }
+  });
+
