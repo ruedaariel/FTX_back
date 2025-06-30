5918bd977c00d96992083362a36920d16b5b60b7
Leandrito12 <leandroandresrueda@gmail.com>
2025-06-30 17:57:15 -0300

    se actualizan directorios y html

--- loginregister/js/login_basico.js
+++ loginregister/js/login_basico.js
@@ -1,35 +1,86 @@
 /*-- ==========================================================================
    Archivo: login_basico.js
-   Descripción: Permite el manejo del modal de error de login_basico.html, redirige la entrada al home del usuario o al home del admin
+   Descripción: Permite el manejo del modal de error de login_basico.html, 
+   redirige la entrada al home del usuario o al home del admin
    ========================================================================== */
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+/* ¿Qué hace esta expresión?
+^ y $ indican el inicio y fin de la cadena.
+(?=.*[a-z]) asegura que haya al menos una letra minúscula.
+(?=.*[A-Z]) asegura que haya al menos una letra mayúscula.
+(?=.*\d) asegura que haya al menos un dígito.
+[A-Za-z\d]{8,} asegura que la contraseña tenga al menos 8 caracteres y solo contenga letras (mayúsculas o minúsculas) y números. */
 
 
-const form = document.querySelector('form');
-        form.addEventListener('submit', function (event) {
-            event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
 
-            /* leo los datos ingresado */
-            const email = document.getElementById('email').value;
-            const password = document.getElementById('password').value;
+// Manejo del modal de error y redirección al home del usuario o del administrador
 
-            /* valido que sea un usuario existente y lo envio a home_usuario*/
-            /* caso contrario muestro modal de error*/
+function validarSesion() {
+  /* leo los datos ingresado */
+  
+  const password = document.getElementById("password").value;
+  const emailInput = document.getElementById("email");
+  const emailValue = emailInput.value;
 
-            if (email === 'mauricio@ejemplo.com' && password === 'mauricio2025') {
-                window.location.href = '../Usuario/home_usuario.html'; // Redirige al home del usuario 
-            } else if (email === 'admin@ejemplo.com' && password === 'admin2025') {
-                window.location.href = '../Administrador/home_administrador.html'; // Redirige al home del administrador
-            } else {
-                const myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
-                
-                myModal.show(); // Muestra el modal
-                
-            }
-        });
+  
 
-        // logica par recargar la pagina al cerrar el modal
+  // Validar formato de email o contrseña no nula
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-        var myModal = document.getElementById('exampleModal')
-        myModal.addEventListener('hidden.bs.modal', function (event) {
-            location.reload();
-        })
+
+  if (!emailRegex.test(emailValue) || password.length === 0 || !passwordRegex.test(password)) {
+    
+    
+    mostrarModal("Error de Validación","Usuario o contraseña incorrectos.","warning",true);
+
+    return; // Detener la ejecución si la validación falla
+  }
+
+  /* valido que sea un usuario existente y lo envio a home_usuario*/
+  /* caso contrario muestro modal de error*/
+
+  if (emailValue === "mauricio@ejemplo.com" && password === "mauricio2025") {
+    window.location.href = "../Usuario/home_usuario.html"; // Redirige al home del usuario
+  } else if (emailValue === "admin@ejemplo.com" && password === "admin2025") {
+    window.location.href = "../Administrador/home_administrador.html"; // Redirige al home del administrador
+  } else {
+
+    mostrarModal("Error de Validación",`${emailValue} no es un usuario valido.`, "warning",true);
+  }
+}
+
+function grabarPerfil (){
+
+  
+mostrarModal("Guardar Perfil","Perfil de Usuario grabado correctamente.", "success",true);
+//window.location.href = "../Usuario/home_usuario.html"; // Redirige al home del usuario
+
+setTimeout(() => {
+    window.location.href = "../Usuario/home_usuario.html"; // Redirige al home del usuario
+  }, 2000); // 2000 milisegundos = 2 segundos
+
+
+
+}
+
+// ==========================================================================
+//  Manejo de Eventos y Lógica de UI
+// ==========================================================================
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const passwordInput = document.getElementById('password');
+    const togglePasswordIcon = document.getElementById('icon-password');
+
+    // El icono inicial ya está en el HTML con la clase fa-eye
+
+    togglePasswordIcon.addEventListener('click', function() {
+        const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordInput.setAttribute('type', type);
+
+        // Alternar las clases de Font Awesome
+        this.classList.toggle('fa-eye');
+        this.classList.toggle('fa-eye-slash');
+    });
+});
