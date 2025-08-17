9fc3cc6ce8ff298a5f88b83de1aa39cbb6eea4af
gladysherrera <profherreratic@gmail.com>
2025-08-17 12:11:42 -0300

    ejercicio-basico completo sin la relacion con rutina - Interceptor

--- back/src/ejercicio-basico/controllers/ejercicio-basico.controller.ts
+++ back/src/ejercicio-basico/controllers/ejercicio-basico.controller.ts
@@ -1,55 +1,62 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile, ParseIntPipe } from '@nestjs/common';
 import { EjercicioBasicoService } from '../services/ejercicio-basico.service';
 import { CreateEjercicioBasicoDto } from '../dto/create-ejercicio-basico.dto';
 import { UpdateEjercicioBasicoDto } from '../dto/update-ejercicio-basico.dto';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
+import { imagenEjercicioInterceptor } from 'src/interceptors/imagen-ejercicio.interceptor';
 
 
 
 @Controller('ejbasico')
 export class EjercicioBasicoController {
   constructor(private readonly ejercicioBasicoService: EjercicioBasicoService) { }
-
-  // @Post()
-  // create(@Body() createEjercicioBasicoDto: CreateEjercicioBasicoDto) {
-  //   return this.ejercicioBasicoService.create(createEjercicioBasicoDto);
-  // }
+  
   @Post('register')
-  @UseInterceptors(FileInterceptor('imagenLink', {
-    storage: diskStorage({
-      destination: './uploads/ejercicios', // Carpeta donde se guarda la imagen
-      filename: (req, file, cb) => {
-        const uniqueName = Date.now() + '-' + file.originalname; // Evita nombres duplicados
-        cb(null, uniqueName);
-      }
-    })
-  }))
+  @UseInterceptors(imagenEjercicioInterceptor()) //ver src/interceptors/imagen-interceptor
   async createEjercicioBasico(
     @UploadedFile() file: Express.Multer.File, // Aquí llega la imagen
     @Body() body: CreateEjercicioBasicoDto // Aquí llegan los datos del ejercicio (nombre, descripción, etc.)
-    ) {
-      body.imagenLink = file?.filename || null;
+  ) {
+    body.imagenLink = file?.filename || null;
     return this.ejercicioBasicoService.createEjercicioBasico(body);
   }
 
-  @Get()
+  @Get('all')
   findAll() {
     return this.ejercicioBasicoService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseIntPipe) id: number) {
     return this.ejercicioBasicoService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEjercicioBasicoDto: UpdateEjercicioBasicoDto) {
+  @Patch('update/:id')
+  @UseInterceptors(imagenEjercicioInterceptor()) //ver src/interceptors/imagen-interceptor
+  update(
+    @Param('id') id: string,
+    @Body() updateEjercicioBasicoDto: UpdateEjercicioBasicoDto,
+    @UploadedFile() file: Express.Multer.File
+  ) {
+    if (file?.filename) {
+      updateEjercicioBasicoDto.imagenLink = file?.filename;
+    }
     return this.ejercicioBasicoService.update(+id, updateEjercicioBasicoDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.ejercicioBasicoService.remove(+id);
+  @Delete('delete/:id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.ejercicioBasicoService.remove(id);
   }
+
+  @Get('name/:name')
+  findByName(@Param('name') nombreEj: string) {
+    return this.ejercicioBasicoService.findByName(nombreEj);
+  }
+
+  @Get('existName/:name')
+  existName(@Param('name') nombreEj: string) {
+    return this.ejercicioBasicoService.existName(nombreEj);
+  }
+
+
 }
