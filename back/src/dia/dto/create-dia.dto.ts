4438e56ff657893795a24a9e8fb4220eeeb1767c
gladysherrera <profherreratic@gmail.com>
2025-09-05 19:51:40 -0300

    correccion de dto para toda la rutina

--- back/src/dia/dto/create-dia.dto.ts
+++ back/src/dia/dto/create-dia.dto.ts
@@ -1,24 +1,31 @@
 // export class CreateDiaDto {}
 // En la carpeta src/dia/dto/create-dia.dto.ts
 
+import { Type } from 'class-transformer';
 import {
   IsString,
   IsNotEmpty,
   IsNumber,
   Length,
+  Matches,
+  ValidateNested,
+  IsArray,
 } from 'class-validator';
+import { CreateEjercicioRutinaDto } from 'src/ejercicio-rutina/dto/create-ejercicio-rutina.dto';
 
 export class CreateDiaDto {
   @IsString({ message: 'El número de día debe ser un texto.' })
   @IsNotEmpty({ message: 'El número de día no puede estar vacío.' })
   @Length(1, 1, { message: 'El número de día debe tener 1 caracter.' })
-  readonly nroDia: string;
+  @Matches(/^[1-7]$/, { message: 'Debe ser un número entre 1 y 7' })
+  nroDia: string;
 
   @IsString({ message: 'El focus debe ser un texto.' })
   @IsNotEmpty({ message: 'El focus no puede estar vacío.' })
-  readonly focus: string;
+  focus: string;
 
-  @IsNumber({}, { message: 'El ID de la semana debe ser un número.' })
-  @IsNotEmpty({ message: 'Debes proporcionar el ID de la semana.' })
-  readonly semanaId: number;
+  @ValidateNested({ each: true })
+  @IsArray()
+  @Type(() => CreateEjercicioRutinaDto)
+  ejerciciosRutina: CreateEjercicioRutinaDto[];
 }
--- back/src/ejercicio-rutina/dto/create-ejercicio-rutina.dto.ts
+++ back/src/ejercicio-rutina/dto/create-ejercicio-rutina.dto.ts
@@ -9,33 +9,30 @@ import {
   IsOptional,
   MaxLength,
   IsPositive,
+  Min,
+  Max,
 } from 'class-validator';
 
 export class CreateEjercicioRutinaDto {
   @IsString({ message: 'Las repeticiones deben ser un texto (ej: "4x12").' })
   @IsNotEmpty({ message: 'Las repeticiones no pueden estar vacías.' })
   @MaxLength(30, { message: 'Las repeticiones no deben exceder los 30 caracteres.' })
-  readonly repeticiones: string;
+  repeticiones: string;
 
   @IsString({ message: 'La dificultad debe ser un texto.' })
   @IsNotEmpty({ message: 'La dificultad no puede estar vacía.' })
   @MaxLength(30, { message: 'La dificultad no debe exceder los 30 caracteres.' })
-  readonly dificultad: string;
+  dificultad: string;
 
   @IsNumber({}, { message: 'El peso debe ser un número.' })
   @IsPositive({ message: 'El peso debe ser un número positivo.' })
   @IsNotEmpty({ message: 'El peso no puede estar vacío.' })
-  readonly peso: number;
+  @Min(0, { message: 'El peso debe ser un numero positivo o 0kg' })
+  @Max(500, { message: 'El peso debe ser como máximo 500kg' })
+  peso: number;
 
   @IsString()
   @IsOptional() // Se marca como opcional porque en la entidad es nullable
-  readonly observaciones?: string;
+  observaciones?: string;
 
-  @IsNumber({}, { message: 'El ID del día debe ser un número.' })
-  @IsNotEmpty({ message: 'Debes proporcionar el ID del día.' })
-  readonly diaId: number;
-
-  @IsNumber({}, { message: 'El ID del ejercicio básico debe ser un número.' })
-  @IsNotEmpty({ message: 'Debes proporcionar el ID del ejercicio básico.' })
-  readonly ejercicioBasicoId: number;
 }
