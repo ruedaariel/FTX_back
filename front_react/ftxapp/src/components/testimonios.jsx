06721e024ae509ea6e876d3822082ecde7a7fac0
ruedaariel <ruedaar@gmail.com>
2025-09-16 09:14:42 -0300

    inicios de landing page en react

--- front_react/ftxapp/src/components/testimonios.jsx
+++ front_react/ftxapp/src/components/testimonios.jsx
@@ -6,70 +6,67 @@ import testimonio3 from "../assets/Recursos/Imagenes/testimonioLea.jpeg";
 export default function Testimonios() {
   return (
     <section id="testimonios">
-      <div class="container text-center">
+      <div className="container text-center">
         <h2>Testimonios</h2>
       </div>
 
       <Carousel interval={3000} indicators={false}>
         <Carousel.Item>
-          <img src={testimonio1} alt="Cliente 1" className="autor-imagen" />
-
-          <Carousel.Caption>
-            <div>
-              <q class="testimonial-text">
-                Desde que empecé con <i>Santi</i>, mi energía ha aumentado y he
-                visto resultados increíbles.
-              </q>
+          <div className="container">
+            <div className="row align-items-center ">
+              <div className="col-md-5 text-center">
+                <img src={testimonio1} alt="Cliente 1" className="autor-imagen" />
+              </div>
+              <div className="col-md-7">
+                <h4>
+                  Desde que empecé con <i>Santi</i>, mi energía ha aumentado y he
+                  visto resultados increíbles.
+                </h4>
+                <span>Agustina</span>
+                <br />
+                <span>Miembro Premium</span>
+              </div>
             </div>
-            <div>
-              <span className="autor-nombre">Agustina</span>
-              <span className="autor-titulo">Miembro Premium</span>
-            </div>
-          </Carousel.Caption>
+          </div>
         </Carousel.Item>
 
-        {/*      <Carousel.Item>
-
-                    <img src={testimonio2} alt="Cliente 1" className="autor-imagen" />
-
-                    <Carousel.Caption>
-                        <div className="text-center">
-                            <q>
-                                La flexibilidad de horarios fue clave para mí. Puedo entrenar sin
-                                estrés y las rutinas son geniales.
-                            </q>
-                            <div>
-                                
-                            </div>
-                        </div>
-
-                        <div>
-                                    <span>Agustina</span>
-                                    <span>Miembro Premium</span>
-                                </div>
-                    </Carousel.Caption>
-                </Carousel.Item>
-
-
                 <Carousel.Item>
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="col-md-4 text-center">
+                <img src={testimonio2} alt="Cliente 2" className="autor-imagen" />
+              </div>
+              <div className="col-md-8">
+                <h5>
+                  La flexibilidad de horarios fue clave para mí. Puedo entrenar
+                  sin estrés y las rutinas son geniales.
+                </h5>
+                <span>Agustina</span>
+                <br />
+                <span>Miembro Premium</span>
+              </div>
+            </div>
+          </div>
+        </Carousel.Item>
 
-                    <img src={testimonio3} alt="Cliente 1" className="autor-imagen" />
-
-                    <Carousel.Caption>
-                        <div className="text-center">
-                            <q>
-                                Para mí es importante entrenar con mi entrenador. Conozco su
-                                profesionalidad y logros.
-                            </q>
-                            <div>
-                                <div>
-                                    <span>Leandro</span>
-                                    <span>Miembro Premium</span>
-                                </div>
-                            </div>
-                        </div>
-                    </Carousel.Caption>
-                </Carousel.Item> */}
+        <Carousel.Item>
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="col-md-4 text-center">
+                <img src={testimonio3} alt="Cliente 3" className="autor-imagen" />
+              </div>
+              <div className="col-md-8">
+                <h5>
+                  Para mí es importante entrenar con mi entrenador. Conozco su
+                  profesionalidad y logros.
+                </h5>
+                <span>Leandro</span>
+                <br />
+                <span>Miembro Premium</span>
+              </div>
+            </div>
+          </div>
+        </Carousel.Item>
       </Carousel>
     </section>
   );
