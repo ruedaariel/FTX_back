bfe5c48684a38f57e66d4643fef0e7055c55e269
gladysherrera <profherreratic@gmail.com>
2025-10-17 20:02:58 -0300

    crud ejercicio basico, modificacion css

--- front_react/ftxapp/src/App.jsx
+++ front_react/ftxapp/src/App.jsx
@@ -1,37 +1,32 @@
-import { useState, useEffect } from "react";
-import {InicioRutina} from "./components/admin/adminRutina/inicioRutina.jsx";
-
-import PantallaClientes from "./components/admin/adminClientes/pantallaClientes/PantallaClientes.jsx";
-/* import AdminApp from './components/admin/adminClientes/'
-import PanelAdmin from './components/PanelAdmin/panelAdmin.jsx'*/
-import HeaderCrud from "./components/componentsShare/header/HeaderCrud.jsx";
-// import { BrowserRouter } from "react-router-dom";
 
 
-// Componentes comentados (no utilizados actualmente)
-/*
-import PantallaClientes from "./components/admin/adminClientes/pantallaClientes/PantallaClientes.jsx";
-import AdminApp from './components/admin/adminClientes/';
-import PanelAdmin from './components/PanelAdmin/panelAdmin.jsx';
-import GestionRutinas from "./components/admin/adminRutina/gestionRutinas/gestionRutinas.jsx";
-import { BrowserRouter } from "react-router-dom";
-*/
+import HeaderCrud from './components/componentsShare/header/HeaderCrud.jsx'
+import CrudEjercicioBasico from './components/admin/adminEjercicio/CrudEjercicioBasico.jsx'
+import { ModalProvider } from "../src/context/ModalContext.jsx";
+import ModalGlobal from "./components/componentsShare/Modal/ModalGlobal.jsx";
 
 function App() {
-  
-
-  
-
-  
-
-  // Render principal
   return (
-    
-      
-      <InicioRutina/>
+    <>
 
+      {/* <Header />
+      <Carousel />
+      <Porqueelegirnos />
+      <Tutrainer />
+      <Planes />
+      <Testimonios />
+      <Faq /> */}
+      {/* <Footer /> */}
+      {/* <LoginApi /> */}
+      <ModalProvider>
+       <HeaderCrud title="Gestion de Ejercicios"></HeaderCrud>
+        <CrudEjercicioBasico></CrudEjercicioBasico>
+      </ModalProvider>
+
+
+
+    </>
   )
-   
 }
 
-export default App;
+export default App
