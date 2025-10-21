069cbe85504c3c7d4e28b2ba0d08f36b67f11b1a
ruedaariel <ruedaar@gmail.com>
2025-10-21 18:48:28 -0300

    se arreglo ruteo

--- front_react/ftxapp/src/App.jsx
+++ front_react/ftxapp/src/App.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { useState, useEffect } from "react";
 
 
@@ -22,38 +22,7 @@ function App() {
       <ModalGlobal />
     </ModalProvider>
   );
-=======
 
-
-import HeaderCrud from './components/componentsShare/header/HeaderCrud.jsx'
-import CrudEjercicioBasico from './components/admin/adminEjercicio/CrudEjercicioBasico.jsx'
-import { ModalProvider } from "../src/context/ModalContext.jsx";
-import ModalGlobal from "./components/componentsShare/Modal/ModalGlobal.jsx";
-
-function App() {
-  return (
-    <>
-
-      {/* <Header />
-      <Carousel />
-      <Porqueelegirnos />
-      <Tutrainer />
-      <Planes />
-      <Testimonios />
-      <Faq /> */}
-      {/* <Footer /> */}
-      {/* <LoginApi /> */}
-      <ModalProvider>
-       <HeaderCrud title="Gestion de Ejercicios"></HeaderCrud>
-        <CrudEjercicioBasico></CrudEjercicioBasico>
-        <ModalGlobal></ModalGlobal>
-      </ModalProvider>
-
-
-
-    </>
-  )
->>>>>>> 787067e90252d34ce5ad2c37bfc75c5fb9cebd77
 }
 
 export default App;
