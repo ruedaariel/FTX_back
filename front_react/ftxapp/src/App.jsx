18937979b8b99323b93c45bfc909b10cf95b80c5
ruedaariel <ruedaar@gmail.com>
2025-10-21 19:00:01 -0300

    se arreglo ap

--- front_react/ftxapp/src/App.jsx
+++ front_react/ftxapp/src/App.jsx
@@ -1,28 +1,31 @@
-
-import { useState, useEffect } from "react";
-
-
-// src/componentsShare/Modal/ModalGlobal.jsx
-
-import { ModalProvider } from "./context/ModalContext.jsx";
+import HeaderCrud from './components/componentsShare/header/HeaderCrud.jsx'
+import CrudEjercicioBasico from './components/admin/adminEjercicio/CrudEjercicioBasico.jsx'
+import { ModalProvider } from "../src/context/ModalContext.jsx";
 import ModalGlobal from "./components/componentsShare/Modal/ModalGlobal.jsx";
-import AppRutas from "./components/rutas/appRutas.jsx";
-import { BrowserRouter } from "react-router-dom";
-import "./styles/style.css";
-import "./styles/colores.css";
 
 function App() {
   return (
-    <ModalProvider>
-      <BrowserRouter>
-        <AppRutas />
-      </BrowserRouter>
+    <>
 
-      {/* <InicioRutina /> */}
-      <ModalGlobal />
-    </ModalProvider>
-  );
+      {/* <Header />
+      <Carousel />
+      <Porqueelegirnos />
+      <Tutrainer />
+      <Planes />
+      <Testimonios />
+      <Faq /> */}
+      {/* <Footer /> */}
+      {/* <LoginApi /> */}
+      <ModalProvider>
+       <HeaderCrud title="Gestion de Ejercicios"></HeaderCrud>
+        <CrudEjercicioBasico></CrudEjercicioBasico>
+        <ModalGlobal></ModalGlobal>
+      </ModalProvider>
 
+
+
+    </>
+  )
 }
 
-export default App;
+export default App
