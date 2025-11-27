8c9815493dff6ddf5c44ddb1d14aad0afc1e3389
ruedaariel <ruedaar@gmail.com>
2025-11-27 19:02:09 -0300

    railway.json

--- back/dist/src/config/data.source.js
+++ back/dist/src/config/data.source.js
@@ -1,17 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppDataSource = void 0;
-const dotenv = require("dotenv");
-dotenv.config({ path: `./.${process.env.MODE_ENV || 'develop'}.env` });
 const typeorm_1 = require("typeorm");
 const typeorm_naming_strategies_1 = require("typeorm-naming-strategies");
+console.log('--- RAILWAY DB CONFIG CHECK ---');
+console.log('HOST:', process.env.MYSQLHOST);
+console.log('USER:', process.env.MYSQLUSER);
+console.log('PASS:', process.env.MYSQLPASSWORD ? '********' : 'NULL (Falta)');
+console.log('PORT:', process.env.MYSQLPORT);
+console.log('DB NAME:', process.env.MYSQL_DATABASE || process.env.MYSQLDATABASE);
+console.log('-------------------------------');
 exports.AppDataSource = new typeorm_1.DataSource({
     type: 'mysql',
-    host: process.env.DB_HOST,
-    port: +(process.env.DB_PORT || '3306'),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    host: process.env.MYSQLHOST || process.env.DB_HOST || 'localhost',
+    port: +(process.env.MYSQLPORT || process.env.DB_PORT || '3306'),
+    username: process.env.MYSQLUSER || process.env.DB_USER,
+    password: process.env.MYSQLPASSWORD || process.env.DB_PASS,
+    database: process.env.MYSQL_DATABASE ||
+        process.env.MYSQLDATABASE ||
+        process.env.DB_NAME,
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     migrations: [__dirname + '/../../migraciones/*{.ts,.js}'],
     synchronize: false,
